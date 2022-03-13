529924eea099ccaf6d6b27b6b3dbf8add99a8976
pwhb <pwhbdev@gmail.com>
2022-03-13 21:30:29 +0630

    display gql data on index page

---
+++ src/lib/gql-client.js
@@ -0,0 +1,2 @@
+import { GraphQLClient } from 'graphql-request';
+export const client = new GraphQLClient(import.meta.env.VITE_GRAPHQL_API);
---
+++ src/routes/index.svelte
@@ -0,0 +1,35 @@
+<script context="module">
+	import { gql } from 'graphql-request';
+	import { client } from '$lib/gql-client';
+	export const load = async () => {
+		const query = gql`
+			query GetProjects {
+				projects {
+					name
+					slug
+					description
+					demo
+					sourceCode
+					image {
+						url
+					}
+				}
+			}
+		`;
+		const { projects } = await client.request(query);
+
+		return {
+			props: {
+				projects
+			}
+		};
+	};
+</script>
+
+<script>
+	export let projects;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<pre>{JSON.stringify(projects, null, 2)}</pre>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
